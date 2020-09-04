47cb5ff306082055058d3b3b999cc3cf5cb3a47c
Diogo Trindade <diogotr7@gmail.com>
2020-09-04 22:05:03 +0100

    Added OpenRGB device plugin

---
+++ Artemis.Plugins.Devices.OpenRGB/Artemis.Plugins.Devices.OpenRGB/OpenRGBDeviceProvider.cs
@@ -0,0 +1,21 @@
+﻿using Artemis.Core.DeviceProviders;
+using Artemis.Core.Services;
+
+namespace Artemis.Plugins.Devices.OpenRGB
+{
+    public class OpenRGBDeviceProvider : DeviceProvider
+    {
+        private readonly IRgbService _rgbService;
+
+        public OpenRGBDeviceProvider(IRgbService rgbService) : base(RGB.NET.Devices.OpenRGB.OpenRGBDeviceProvider.Instance)
+        {
+            _rgbService = rgbService;
+        }
+
+        public override void EnablePlugin()
+        {
+            RGB.NET.Devices.OpenRGB.OpenRGBDeviceProvider.ClientName = "Artemis";
+            _rgbService.AddDeviceProvider(RgbDeviceProvider);
+        }
+    }
+}
---
+++ Artemis.Plugins.Devices.OpenRGB/RgbNetDeviceProvider/MyPluginDeviceProviderLoader.cs
@@ -0,0 +1,13 @@
+﻿using RGB.NET.Core;
+
+namespace RgbNetDeviceProvider
+{
+    // This is your RGB.NET device provider loader
+    // See https://github.com/DarthAffe/RGB.NET for more info on RGB.NET and examples of other device providers
+    public class MyPluginDeviceProviderLoader : IRGBDeviceProviderLoader
+    {
+        // Set to true if you need some kind of initialization in your device provider
+        public bool RequiresInitialization => false;
+        public IRGBDeviceProvider GetDeviceProvider() => MyPluginDeviceProvider.Instance;
+    }
+}
