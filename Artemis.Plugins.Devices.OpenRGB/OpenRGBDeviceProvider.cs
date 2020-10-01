b7f8b8ca972240b12ed4d4642ca6f39d427ad8d5
Diogo Trindade <diogotr7@gmail.com>
2020-10-02 00:00:08 +0100

    OpenRGB: added support for multiple servers

---
+++ Artemis.Plugins.Devices.OpenRGB/OpenRGBDeviceProvider.cs
@@ -0,0 +1,46 @@
+﻿using Artemis.Core;
+using Artemis.Core.DeviceProviders;
+using Artemis.Core.Services;
+using RGB.NET.Devices.OpenRGB;
+using System.Collections.Generic;
+
+namespace Artemis.Plugins.Devices.OpenRGB
+{
+    public class OpenRGBDeviceProvider : DeviceProvider
+    {
+        private readonly IRgbService _rgbService;
+        private readonly PluginSettings _settings;
+
+        public OpenRGBDeviceProvider(IRgbService rgbService, PluginSettings settings) : base(RGB.NET.Devices.OpenRGB.OpenRGBDeviceProvider.Instance)
+        {
+            _rgbService = rgbService;
+            _settings = settings;
+        }
+
+        public override void EnablePlugin()
+        {
+            ConfigurationDialog = new PluginConfigurationDialog<OpenRGBConfigurationDialogViewModel>();
+
+            var definitions = _settings.GetSetting<List<OpenRGBServerDefinition>>("DeviceDefinitions");
+            if (definitions.Value is null)
+            {
+                definitions.Value = new List<OpenRGBServerDefinition>();
+                var definition = new OpenRGBServerDefinition
+                {
+                    ClientName = "Artemis",
+                    Ip = "127.0.0.1",
+                    Port = 6742
+                };
+                definitions.Value.Add(definition);
+                definitions.Save();
+            }
+
+            foreach (var def in definitions.Value)
+            {
+                RGB.NET.Devices.OpenRGB.OpenRGBDeviceProvider.Instance.DeviceDefinitions.Add(def);
+            }
+
+            _rgbService.AddDeviceProvider(RgbDeviceProvider);
+        }
+    }
+}
