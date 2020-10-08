5656b33df3295550b73bb3bd50e4f3490fadb92b
Diogo Trindade <diogotr7@gmail.com>
2020-10-08 15:16:32 +0100

    initial obs plugin

---
+++ Artemis.Plugins.DataModelExpansions.OBS/DataModels/ObsDataModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Artemis.Core.DataModelExpansions;
+using OBS.WebSocket.NET;
+using OBS.WebSocket.NET.Types;
+
+namespace Artemis.Plugins.DataModelExpansions.OBS.DataModels
+{
+    public class ObsDataModel : DataModel
+    {
+        public bool IsConnected { get; set; }
+        public string CurrentProfile { get; set; }
+        public string CurrentScene { get; set; }
+        public bool Streaming { get; set; }
+        public int TotalStreamTime { get; set; }
+        public ulong TotalStreamBytes { get; set; }
+        public ulong TotalStreamFrames { get; set; }
+        public bool Recording { get; set; }
+        public bool Paused { get; set; }
+        public int TotalRecordTime { get; set; }
+        public int TotalTecordBytes { get; set; }
+        public int TotalRecordFrames { get; set; }
+        public OBSStats Stats { get; set; }
+    }
+}
---
+++ Artemis.Plugins.DataModelExpansions.OBS/ObsDataModelExpansion.cs
@@ -0,0 +1,63 @@
+﻿using Artemis.Core.DataModelExpansions;
+using Artemis.Plugins.DataModelExpansions.OBS.DataModels;
+using OBS.WebSocket.NET;
+using OBS.WebSocket.NET.Types;
+using System;
+using System.Linq;
+
+namespace Artemis.Plugins.DataModelExpansions.OBS
+{
+    public class ObsDataModelExpansion : DataModelExpansion<ObsDataModel>
+    {
+        private const string OBS_URI = "ws://127.0.0.1:4444";
+        private const string OBS_PASSWORD = "";
+
+        private ObsWebSocket _obs;
+
+        public override void EnablePlugin()
+        {
+            _obs = new ObsWebSocket();
+
+            try
+            {
+                _obs.Connect(OBS_URI, OBS_PASSWORD);
+                _obs.Api.SetHeartbeat(true);
+                _obs.Heartbeat += UpdateHeartbeat;
+
+                DataModel.IsConnected = true;
+            }
+            catch
+            {
+                DataModel.IsConnected = false;
+                //logger
+            }
+        }
+
+        public override void DisablePlugin()
+        {
+            _obs.Heartbeat -= UpdateHeartbeat;
+            _obs?.Disconnect();
+        }
+
+        public override void Update(double deltaTime)
+        {
+
+        }
+
+        private void UpdateHeartbeat(ObsWebSocket sender, Heartbeat heatbeat)
+        {
+            DataModel.CurrentProfile = heatbeat.CurrentProfile;
+            DataModel.CurrentScene = heatbeat.CurrentScene;
+            DataModel.Streaming = heatbeat.Streaming;
+            DataModel.TotalStreamTime = heatbeat.TotalStreamTime;
+            DataModel.TotalStreamBytes = heatbeat.TotalStreamBytes;
+            DataModel.TotalStreamFrames = heatbeat.TotalStreamFrames;
+            DataModel.Recording = heatbeat.Recording;
+            DataModel.Paused = heatbeat.Paused;
+            DataModel.TotalRecordTime = heatbeat.TotalRecordTime;
+            DataModel.TotalTecordBytes = heatbeat.TotalTecordBytes;
+            DataModel.TotalRecordFrames = heatbeat.TotalRecordFrames;
+            DataModel.Stats = heatbeat.Stats;
+        }
+    }
+}
