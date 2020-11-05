7be0e4ce0491ca38caef032aba6bb7a0c47608d6
Diogo Trindade <diogotr7@gmail.com>
2020-11-05 01:51:40 +0000

    initial fallout scaffolding

---
+++ src/Artemis.Plugins.Modules.Fallout4/DataModels/Fallout4DataModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Artemis.Core.DataModelExpansions;
+
+namespace Artemis.Plugins.Modules.Fallout4.DataModels
+{
+    public class Fallout4DataModel : DataModel
+    {
+
+    }
+}
---
+++ src/Artemis.Plugins.Modules.Fallout4/Fallout4Module.cs
@@ -0,0 +1,36 @@
+﻿using Artemis.Core;
+using Artemis.Core.Modules;
+using SkiaSharp;
+using Artemis.Plugins.Modules.Fallout4.DataModels;
+
+namespace Artemis.Plugins.Modules.Fallout4
+{
+    public class Fallout4Module : ProfileModule<Fallout4DataModel>
+    {
+        public override void EnablePlugin()
+        {
+            DisplayName = "Fallout 4";
+            DisplayIcon = "ToyBrickPlus";
+            DefaultPriorityCategory = ModulePriorityCategory.Application;
+            UpdateDuringActivationOverride = false;
+        }
+
+        public override void DisablePlugin()
+        {
+        }
+
+        public override void ModuleActivated(bool isOverride)
+        {
+        }
+
+        public override void ModuleDeactivated(bool isOverride)
+        {
+        }
+
+        public override void Update(double deltaTime)
+        {
+        }
+
+        public override void Render(double deltaTime, ArtemisSurface surface, SKCanvas canvas, SKImageInfo canvasInfo) { }
+    }
+}
