42e3076f4f281c0751b6d390003e63f1768e52a7
Diogo Trindade <diogotr7@gmail.com>
2021-01-29 01:33:26 +0000

    Fixed NRE when joining a VC

--- src/Artemis.Plugins.DataModelExpansions.Discord/DataModels/DiscordDataModel.cs
+++ src/Artemis.Plugins.DataModelExpansions.Discord/DataModels/DiscordDataModel.cs
@@ -5,55 +5,41 @@ namespace Artemis.Plugins.DataModelExpansions.Discord.DataModels
 {
     public class DiscordDataModel : DataModel
     {
-        [DataModelProperty]
         public DiscordUserDataModel User { get; set; } = new DiscordUserDataModel();
 
-        [DataModelProperty]
         public DiscordVoiceSettingsDataModel VoiceSettings { get; set; } = new DiscordVoiceSettingsDataModel();
 
-        [DataModelProperty]
         public DiscordVoiceConnectionStatusDataModel VoiceConnection { get; set; } = new DiscordVoiceConnectionStatusDataModel();
 
-        [DataModelProperty]
         public DataModelEvent Notification { get; set; } = new DataModelEvent();
     }
 
-    public class DiscordVoiceSettingsDataModel
+    public class DiscordVoiceSettingsDataModel : DataModel
     {
-        [DataModelProperty]
         public bool Muted { get; set; }
 
-        [DataModelProperty]
         public bool Deafened { get; set; }
 
-        [DataModelProperty]
         public bool Speaking { get; set; }
     }
 
-    public class DiscordUserDataModel
+    public class DiscordUserDataModel : DataModel
     {
-        [DataModelProperty]
         public string Username { get; set; }
 
-        [DataModelProperty]
         public string Discriminator { get; set; }
 
-        [DataModelProperty]
         public string Id { get; set; }
     }
 
-    public class DiscordVoiceConnectionStatusDataModel
+    public class DiscordVoiceConnectionStatusDataModel : DataModel
     {
-        [DataModelProperty]
         public float? Ping { get; set; }
 
-        [DataModelProperty]
         public string State { get; set; }
 
-        [DataModelProperty]
-        public DataModelEvent Connected { get; set; }
+        public DataModelEvent Connected { get; set; } = new DataModelEvent();
 
-        [DataModelProperty]
-        public DataModelEvent Disconnected { get; set; }
+        public DataModelEvent Disconnected { get; set; } = new DataModelEvent();
     }
 }
