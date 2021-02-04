0da304a8c9ee89d6119ac349e5f1c24560f3aa47
Diogo Trindade <diogotr7@gmail.com>
2021-02-04 16:49:48 +0000

    Ambilight - Added GPU resizing

--- src/Artemis.Plugins.LayerBrushes.Ambilight/PluginLayerBrush.cs
+++ src/Artemis.Plugins.LayerBrushes.Ambilight/PluginLayerBrush.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using Vortice.Direct3D;
 using Vortice.Direct3D11;
 using Vortice.DXGI;
+using Usage = Vortice.Direct3D11.Usage;
 
 namespace Artemis.Plugins.LayerBrushes.Ambilight
 {
@@ -23,37 +24,61 @@ namespace Artemis.Plugins.LayerBrushes.Ambilight
         };
 
         private IDXGIOutputDuplication duplication;
-        private ID3D11Texture2D texture;
+        private ID3D11Texture2D stagingTexture;
         private ID3D11Device device;
-        private SKBitmap skBitmap = new SKBitmap();
+        private SKBitmap skBitmap = new SKBitmap(1920 / 2, 1080 / 2);
         double time;
+        private ID3D11Texture2D smallerTexture;
+        private ID3D11ShaderResourceView smallerTextureView;
 
         public override void EnableLayerBrush()
         {
             var factory = DXGI.CreateDXGIFactory1<IDXGIFactory1>();
-            var adapter = factory.GetAdapter(0);
+            var adapter = factory.GetAdapter1(0);
+
+            D3D11.D3D11CreateDevice(adapter, DriverType.Unknown, DeviceCreationFlags.None, s_featureLevels, out device);
+
             var output = adapter.GetOutput(0);
             var output1 = output.QueryInterface<IDXGIOutput1>();
-            D3D11.D3D11CreateDevice(adapter, DriverType.Unknown, DeviceCreationFlags.Debug, s_featureLevels, out device);
 
             var bounds = output1.Description.DesktopCoordinates;
+            var width = bounds.Right - bounds.Left;
+            var height = bounds.Bottom - bounds.Top;
+
             var textureDesc = new Texture2DDescription
             {
                 CpuAccessFlags = CpuAccessFlags.Read,
                 BindFlags = BindFlags.None,
                 Format = Format.B8G8R8A8_UNorm,
-                Width = bounds.Right - bounds.Left,
-                Height = bounds.Bottom - bounds.Top,
+                Width = width / 2,
+                Height = height / 2,
                 OptionFlags = ResourceOptionFlags.None,
                 MipLevels = 1,
                 ArraySize = 1,
                 SampleDescription = { Count = 1, Quality = 0 },
-                Usage = Vortice.Direct3D11.Usage.Staging
+                Usage = Usage.Staging
+            };
+            stagingTexture = device.CreateTexture2D(textureDesc);
+
+            var smallerTextureDesc = new Texture2DDescription
+            {
+                CpuAccessFlags = CpuAccessFlags.None,
+                BindFlags = BindFlags.RenderTarget | BindFlags.ShaderResource,
+                Format = Format.B8G8R8A8_UNorm,
+                Width = width,
+                Height = height,
+                OptionFlags = ResourceOptionFlags.GenerateMips,
+                MipLevels = 4,
+                ArraySize = 1,
+                SampleDescription = { Count = 1, Quality = 0 },
+                Usage = Usage.Default
             };
 
-            duplication = output1.DuplicateOutput(device);
-            texture = device.CreateTexture2D(textureDesc);
+            smallerTexture = device.CreateTexture2D(smallerTextureDesc);
+            smallerTextureView = device.CreateShaderResourceView(smallerTexture);
 
+            duplication = output1.DuplicateOutput(device);
+            
             Thread.Sleep(100);
         }
 
@@ -71,30 +96,39 @@ namespace Artemis.Plugins.LayerBrushes.Ambilight
             //else
             //{
             //    time = 0;
-            //}
+            //}\
+            IDXGIResource screenResource;
+            OutduplFrameInfo frameInfo;
 
-            duplication.AcquireNextFrame(500, out var frameInfo, out var desktopResource);
-            using (desktopResource)
-            {
-                using var tempTexture = desktopResource.QueryInterface<ID3D11Texture2D>();
-                device.ImmediateContext.CopyResource(texture, tempTexture);
-            }
+            duplication.AcquireNextFrame(500, out frameInfo, out screenResource);
 
-            duplication?.ReleaseFrame();
+            using (var tempTexture = screenResource.QueryInterface<ID3D11Texture2D>())
+                device.ImmediateContext.CopySubresourceRegion(smallerTexture, 0, 0, 0, 0, tempTexture, 0);
 
-            var dataBox = device.ImmediateContext.Map(texture, 0, MapMode.Read, Vortice.Direct3D11.MapFlags.None);
+            device.ImmediateContext.GenerateMips(smallerTextureView);
+            device.ImmediateContext.CopySubresourceRegion(stagingTexture, 0, 0, 0, 0, smallerTexture, 1);
+
+            var dataBox = device.ImmediateContext.Map(stagingTexture, 0, MapMode.Read, Vortice.Direct3D11.MapFlags.None);
 
             ProcessDataIntoSKBitmap(dataBox);
+
+            device.ImmediateContext.Unmap(stagingTexture, 0);
+
+            screenResource.Dispose();
+            duplication?.ReleaseFrame();
         }
 
         private void ProcessDataIntoSKBitmap(MappedSubresource dataBox)
         {
+            int width = dataBox.RowPitch / 4;
+            int height = dataBox.DepthPitch / width / 4;
+
             var skInfo = new SKImageInfo
             {
                 ColorType = SKColorType.Bgra8888,
                 AlphaType = SKAlphaType.Premul,
-                Height = 1080,
-                Width = 1920
+                Height = height,
+                Width = width
             };
             var skPixmap = new SKPixmap(skInfo, dataBox.DataPointer);
             skBitmap.InstallPixels(skPixmap);
