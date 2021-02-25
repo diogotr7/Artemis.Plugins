4b7a1ac4c073935d35453ccd631e47c07fc40c5a
Diogo Trindade <diogotr7@gmail.com>
2021-02-25 14:58:44 +0000

    Ambilight - small improvements

--- src/Artemis.Plugins.LayerBrushes.Ambilight/AmbilightLayerBrush.cs
+++ src/Artemis.Plugins.LayerBrushes.Ambilight/AmbilightLayerBrush.cs
@@ -1,4 +1,4 @@
-using Artemis.Core;
+﻿using Artemis.Core;
 using Artemis.Core.LayerBrushes;
 using Artemis.Plugins.LayerBrushes.Ambilight.PropertyGroups;
 using SkiaSharp;
@@ -30,6 +30,7 @@ namespace Artemis.Plugins.LayerBrushes.Ambilight
         private ID3D11Device device;
         private ID3D11Texture2D smallerTexture;
         private ID3D11ShaderResourceView smallerTextureView;
+        private bool release = false;
 
         public override void EnableLayerBrush()
         {
@@ -39,55 +40,9 @@ namespace Artemis.Plugins.LayerBrushes.Ambilight
             Thread.Sleep(100);
         }
 
-        private void StartDesktopDuplicator(int adapterId, int outputId)
-        {
-            var adapter = factory.GetAdapter1(adapterId);
-
-            D3D11.D3D11CreateDevice(adapter, DriverType.Unknown, DeviceCreationFlags.None, s_featureLevels, out device);
-
-            var output = adapter.GetOutput(outputId);
-            var output1 = output.QueryInterface<IDXGIOutput1>();
-
-            var bounds = output1.Description.DesktopCoordinates;
-            var width = bounds.Right - bounds.Left;
-            var height = bounds.Bottom - bounds.Top;
-
-            var textureDesc = new Texture2DDescription
-            {
-                CpuAccessFlags = CpuAccessFlags.Read,
-                BindFlags = BindFlags.None,
-                Format = Format.B8G8R8A8_UNorm,
-                Width = width / 2,
-                Height = height / 2,
-                OptionFlags = ResourceOptionFlags.None,
-                MipLevels = 1,
-                ArraySize = 1,
-                SampleDescription = { Count = 1, Quality = 0 },
-                Usage = Usage.Staging
-            };
-            stagingTexture = device.CreateTexture2D(textureDesc);
-
-            var smallerTextureDesc = new Texture2DDescription
-            {
-                CpuAccessFlags = CpuAccessFlags.None,
-                BindFlags = BindFlags.RenderTarget | BindFlags.ShaderResource,
-                Format = Format.B8G8R8A8_UNorm,
-                Width = width,
-                Height = height,
-                OptionFlags = ResourceOptionFlags.GenerateMips,
-                MipLevels = 4,
-                ArraySize = 1,
-                SampleDescription = { Count = 1, Quality = 0 },
-                Usage = Usage.Default
-            };
-            smallerTexture = device.CreateTexture2D(smallerTextureDesc);
-            smallerTextureView = device.CreateShaderResourceView(smallerTexture);
-
-            duplication = output1.DuplicateOutput(device);
-        }
-
         public override void DisableLayerBrush()
         {
+            StopDesktopDuplicator();
         }
 
         public override void Update(double deltaTime)
@@ -96,6 +51,12 @@ namespace Artemis.Plugins.LayerBrushes.Ambilight
             OutduplFrameInfo frameInfo;
             try
             {
+                if (release)
+                {
+                    duplication.ReleaseFrame();
+                    release = false;
+                }
+
                 duplication.AcquireNextFrame(500, out frameInfo, out screenResource);
 
                 using (var tempTexture = screenResource.QueryInterface<ID3D11Texture2D>())
@@ -111,14 +72,14 @@ namespace Artemis.Plugins.LayerBrushes.Ambilight
                 device.ImmediateContext.Unmap(stagingTexture, 0);
 
                 screenResource.Dispose();
-                duplication.ReleaseFrame();
+                release = true;
+                //duplication.ReleaseFrame();
             }
             catch (SharpGen.Runtime.SharpGenException e)
             {
                 if(e.ResultCode == Vortice.DXGI.ResultCode.AccessLost)
                 {
                     StartDesktopDuplicator(0, 0);
-
                 }
                 else if(e.ResultCode == Vortice.DXGI.ResultCode.WaitTimeout)
                 {
@@ -186,6 +147,63 @@ namespace Artemis.Plugins.LayerBrushes.Ambilight
                 }
                 return new SKColor((byte)(r / sampleSize), (byte)(g / sampleSize), (byte)(b / sampleSize));
             }
+        }
+
+        //adapted from https://stackoverflow.com/questions/24064837/resizing-a-dxgi-resource-or-texture2d-in-sharpdx
+        private void StartDesktopDuplicator(int adapterId, int outputId)
+        {
+            var adapter = factory.GetAdapter1(adapterId);
+
+            D3D11.D3D11CreateDevice(adapter, DriverType.Unknown, DeviceCreationFlags.Debug, s_featureLevels, out device);
+
+            var output = adapter.GetOutput(outputId);
+            var output1 = output.QueryInterface<IDXGIOutput1>();
+
+            var bounds = output1.Description.DesktopCoordinates;
+            var width = bounds.Right - bounds.Left;
+            var height = bounds.Bottom - bounds.Top;
+
+            var textureDesc = new Texture2DDescription
+            {
+                CpuAccessFlags = CpuAccessFlags.Read,
+                BindFlags = BindFlags.None,
+                Format = Format.B8G8R8A8_UNorm,
+                Width = width / 2,
+                Height = height / 2,
+                OptionFlags = ResourceOptionFlags.None,
+                MipLevels = 1,
+                ArraySize = 1,
+                SampleDescription = { Count = 1, Quality = 0 },
+                Usage = Usage.Staging
+            };
+            stagingTexture = device.CreateTexture2D(textureDesc);
+
+            var smallerTextureDesc = new Texture2DDescription
+            {
+                CpuAccessFlags = CpuAccessFlags.None,
+                BindFlags = BindFlags.RenderTarget | BindFlags.ShaderResource,
+                Format = Format.B8G8R8A8_UNorm,
+                Width = width,
+                Height = height,
+                OptionFlags = ResourceOptionFlags.GenerateMips,
+                MipLevels = 4,
+                ArraySize = 1,
+                SampleDescription = { Count = 1, Quality = 0 },
+                Usage = Usage.Default
+            };
+            smallerTexture = device.CreateTexture2D(smallerTextureDesc);
+            smallerTextureView = device.CreateShaderResourceView(smallerTexture);
+
+            duplication = output1.DuplicateOutput(device);
+        }
+
+        private void StopDesktopDuplicator()
+        {
+            device?.Dispose();
+            stagingTexture?.Dispose();
+            smallerTexture?.Dispose();
+            smallerTextureView?.Dispose();
+            duplication.Dispose();
         }
     }
 }
