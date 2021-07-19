5c580b592e8e6503d3ba15430edcde495fae08ca
Diogo Trindade <diogotr7@gmail.com>
2021-07-19 17:26:48 +0100

    Added practice tool name

--- src/Artemis.Plugins.Modules.LeagueOfLegends/DataModels/Enums/GameMode.cs
+++ src/Artemis.Plugins.Modules.LeagueOfLegends/DataModels/Enums/GameMode.cs
@@ -1,10 +1,13 @@
-﻿namespace Artemis.Plugins.Modules.LeagueOfLegends.DataModels.Enums
+﻿using Artemis.Plugins.Modules.LeagueOfLegends.Utils;
+
+namespace Artemis.Plugins.Modules.LeagueOfLegends.DataModels.Enums
 {
     //TODO: Find the rest of these
     public enum GameMode
     {
         Unknown = -1,
         None = 0,
+        [Name("PRACTICETOOL")]
         PracticeTool,
         Classic
     }
