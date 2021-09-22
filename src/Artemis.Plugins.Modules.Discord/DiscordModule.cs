10d2d299d72cc52c19981fcbc71846925f375c6b
Diogo Trindade <diogotr7@gmail.com>
2021-09-22 14:11:00 +0100

    Catch discord event exceptions otherwise we crash Artemis

--- src/Artemis.Plugins.Modules.Discord/DiscordModule.cs
+++ src/Artemis.Plugins.Modules.Discord/DiscordModule.cs
@@ -90,122 +90,129 @@ namespace Artemis.Plugins.Modules.Discord
 
         private async void OnDiscordEventReceived(object sender, DiscordEvent discordEvent)
         {
-            switch (discordEvent)
+            try
             {
-                case DiscordEvent<ReadyData>:
-                    if (_token.Value == null)
-                    {
-                        //We have no token saved. This means it's probably the first time
-                        //the user is using the plugin. We need to ask for their permission
-                        //to get a token from discord. 
-                        //This token can be saved and reused (+ refreshed) later.
-                        var authorizeResponse = await discordClient.SendRequest<AuthorizeData>(
-                            new DiscordRequest(DiscordRpcCommand.AUTHORIZE)
-                                .WithArgument("client_id", _clientId.Value)
-                                .WithArgument("scopes", SCOPES));
-
-                        SaveToken(await GetAccessTokenAsync(authorizeResponse.Data.Code));
-                    }
-                    else
-                    {
-                        //If we already have a token saved from earlier,
-                        //we need to check if it expired or not.
-                        //If yes, refresh it.
-                        //Then, authenticate.
-                        if (_token.Value.ExpirationDate < DateTime.UtcNow)
+                switch (discordEvent)
+                {
+                    case DiscordEvent<ReadyData>:
+                        if (_token.Value == null)
                         {
-                            SaveToken(await RefreshAccessTokenAsync(_token.Value.RefreshToken));
+                            //We have no token saved. This means it's probably the first time
+                            //the user is using the plugin. We need to ask for their permission
+                            //to get a token from discord. 
+                            //This token can be saved and reused (+ refreshed) later.
+                            var authorizeResponse = await discordClient.SendRequest<AuthorizeData>(
+                                new DiscordRequest(DiscordRpcCommand.AUTHORIZE)
+                                    .WithArgument("client_id", _clientId.Value)
+                                    .WithArgument("scopes", SCOPES));
+
+                            SaveToken(await GetAccessTokenAsync(authorizeResponse.Data.Code));
                         }
-                    }
-
-                    var authenticateResponse =
-                        await discordClient.SendRequest<AuthenticateData>(
-                            new DiscordRequest(DiscordRpcCommand.AUTHENTICATE)
-                                .WithArgument("access_token", _token.Value.AccessToken));
-
-                    DataModel.User.Username = authenticateResponse.Data.User.Username;
-                    DataModel.User.Discriminator = authenticateResponse.Data.User.Discriminator;
-                    DataModel.User.Id = authenticateResponse.Data.User.Id;
-
-                    //Initial request for data, then use events after
-                    var voiceSettingsResponse =
-                        await discordClient.SendRequest<VoiceSettingsData>(
-                            new DiscordRequest(DiscordRpcCommand.GET_VOICE_SETTINGS));
-
-                    DataModel.VoiceSettings.Deafened = voiceSettingsResponse.Data.Deaf;
-                    DataModel.VoiceSettings.Muted = voiceSettingsResponse.Data.Mute;
-
-                    await UpdateVoiceChannelData();
-
-                    //Subscribe to these events as well
-                    await discordClient.SendRequestAsync(new DiscordSubscribe(DiscordRpcEvent.VOICE_SETTINGS_UPDATE));
-                    await discordClient.SendRequestAsync(new DiscordSubscribe(DiscordRpcEvent.NOTIFICATION_CREATE));
-                    await discordClient.SendRequestAsync(new DiscordSubscribe(DiscordRpcEvent.VOICE_CONNECTION_STATUS));
-                    await discordClient.SendRequestAsync(new DiscordSubscribe(DiscordRpcEvent.VOICE_CHANNEL_SELECT));
-                    break;
-                case DiscordEvent<VoiceSettingsData> voice:
-                    var voiceData = voice.Data;
-                    DataModel.VoiceSettings.AutomaticGainControl = voiceData.AutomaticGainControl;
-                    DataModel.VoiceSettings.EchoCancellation = voiceData.EchoCancellation;
-                    DataModel.VoiceSettings.NoiseSuppression = voiceData.NoiseSuppression;
-                    DataModel.VoiceSettings.Qos = voiceData.Qos;
-                    DataModel.VoiceSettings.SilenceWarning = voiceData.SilenceWarning;
-                    DataModel.VoiceSettings.Deafened = voiceData.Deaf;
-                    DataModel.VoiceSettings.Muted = voiceData.Mute;
-                    DataModel.VoiceSettings.Mode.Type = Enum.Parse<DiscordVoiceModeType>(voiceData.Mode.Type);
-                    DataModel.VoiceSettings.Mode.AutoThreshold = voiceData.Mode.AutoThreshold;
-                    DataModel.VoiceSettings.Mode.Threshold = voiceData.Mode.Threshold;
-                    DataModel.VoiceSettings.Mode.Shortcut = voiceData.Mode.Shortcut
-                        .Select(ds => new DiscordShortcut
+                        else
                         {
-                            Type = (DiscordKeyType)ds.Type,
-                            Code = ds.Code,
-                            Name = ds.Name
-                        })
-                        .ToArray();
+                            //If we already have a token saved from earlier,
+                            //we need to check if it expired or not.
+                            //If yes, refresh it.
+                            //Then, authenticate.
+                            if (_token.Value.ExpirationDate < DateTime.UtcNow)
+                            {
+                                SaveToken(await RefreshAccessTokenAsync(_token.Value.RefreshToken));
+                            }
+                        }
 
-                    break;
-                case DiscordEvent<VoiceConnectionStatusData> voiceStatus:
-                    DataModel.VoiceConnection.State = voiceStatus.Data.State;
-                    DataModel.VoiceConnection.Ping = voiceStatus.Data.LastPing;
-                    DataModel.VoiceConnection.Hostname = voiceStatus.Data.Hostname;
-                    break;
-                case DiscordEvent<NotificationCreateData> notif:
-                    DataModel.Notification.Trigger(new DiscordNotificationEventArgs
-                    {
-                        Body = notif.Data.Body,
-                        ChannelId = notif.Data.ChannelId,
-                        IconUrl = notif.Data.IconUrl,
-                        Author = notif.Data.Message.Author,
-                        Title = notif.Data.Title
-                    });
-                    break;
-                case DiscordEvent<SpeakingStopData> speakingStop:
-                    if (speakingStop.Data.UserId == DataModel.User.Id)
-                    {
-                        DataModel.VoiceConnection.Speaking = false;
-                    }
-                    break;
-                case DiscordEvent<SpeakingStartData> speakingStart:
-                    if (speakingStart.Data.UserId == DataModel.User.Id)
-                    {
-                        DataModel.VoiceConnection.Speaking = true;
-                    }
-                    break;
-                case DiscordEvent<VoiceChannelSelectData> voiceSelect:
-                    if (voiceSelect.Data.ChannelId is not null)//join voice channel
-                    {
-                        DataModel.VoiceConnection.Connected.Trigger();
+                        var authenticateResponse =
+                            await discordClient.SendRequest<AuthenticateData>(
+                                new DiscordRequest(DiscordRpcCommand.AUTHENTICATE)
+                                    .WithArgument("access_token", _token.Value.AccessToken));
+
+                        DataModel.User.Username = authenticateResponse.Data.User.Username;
+                        DataModel.User.Discriminator = authenticateResponse.Data.User.Discriminator;
+                        DataModel.User.Id = authenticateResponse.Data.User.Id;
+
+                        //Initial request for data, then use events after
+                        var voiceSettingsResponse =
+                            await discordClient.SendRequest<VoiceSettingsData>(
+                                new DiscordRequest(DiscordRpcCommand.GET_VOICE_SETTINGS));
+
+                        DataModel.VoiceSettings.Deafened = voiceSettingsResponse.Data.Deaf;
+                        DataModel.VoiceSettings.Muted = voiceSettingsResponse.Data.Mute;
+
                         await UpdateVoiceChannelData();
-                    }
-                    else//leave voice channel
-                    {
-                        DataModel.VoiceConnection.Disconnected.Trigger();
-                    }
-                    break;
-                default:
-                    _logger.Error("Received unexpected discord event of type {eventType}", discordEvent.Event);
-                    break;
+
+                        //Subscribe to these events as well
+                        await discordClient.SendRequestAsync(new DiscordSubscribe(DiscordRpcEvent.VOICE_SETTINGS_UPDATE));
+                        await discordClient.SendRequestAsync(new DiscordSubscribe(DiscordRpcEvent.NOTIFICATION_CREATE));
+                        await discordClient.SendRequestAsync(new DiscordSubscribe(DiscordRpcEvent.VOICE_CONNECTION_STATUS));
+                        await discordClient.SendRequestAsync(new DiscordSubscribe(DiscordRpcEvent.VOICE_CHANNEL_SELECT));
+                        break;
+                    case DiscordEvent<VoiceSettingsData> voice:
+                        var voiceData = voice.Data;
+                        DataModel.VoiceSettings.AutomaticGainControl = voiceData.AutomaticGainControl;
+                        DataModel.VoiceSettings.EchoCancellation = voiceData.EchoCancellation;
+                        DataModel.VoiceSettings.NoiseSuppression = voiceData.NoiseSuppression;
+                        DataModel.VoiceSettings.Qos = voiceData.Qos;
+                        DataModel.VoiceSettings.SilenceWarning = voiceData.SilenceWarning;
+                        DataModel.VoiceSettings.Deafened = voiceData.Deaf;
+                        DataModel.VoiceSettings.Muted = voiceData.Mute;
+                        DataModel.VoiceSettings.Mode.Type = Enum.Parse<DiscordVoiceModeType>(voiceData.Mode.Type);
+                        DataModel.VoiceSettings.Mode.AutoThreshold = voiceData.Mode.AutoThreshold;
+                        DataModel.VoiceSettings.Mode.Threshold = voiceData.Mode.Threshold;
+                        DataModel.VoiceSettings.Mode.Shortcut = voiceData.Mode.Shortcut
+                            .Select(ds => new DiscordShortcut
+                            {
+                                Type = (DiscordKeyType)ds.Type,
+                                Code = ds.Code,
+                                Name = ds.Name
+                            })
+                            .ToArray();
+
+                        break;
+                    case DiscordEvent<VoiceConnectionStatusData> voiceStatus:
+                        DataModel.VoiceConnection.State = voiceStatus.Data.State;
+                        DataModel.VoiceConnection.Ping = voiceStatus.Data.LastPing;
+                        DataModel.VoiceConnection.Hostname = voiceStatus.Data.Hostname;
+                        break;
+                    case DiscordEvent<NotificationCreateData> notif:
+                        DataModel.Notification.Trigger(new DiscordNotificationEventArgs
+                        {
+                            Body = notif.Data.Body,
+                            ChannelId = notif.Data.ChannelId,
+                            IconUrl = notif.Data.IconUrl,
+                            Author = notif.Data.Message.Author,
+                            Title = notif.Data.Title
+                        });
+                        break;
+                    case DiscordEvent<SpeakingStopData> speakingStop:
+                        if (speakingStop.Data.UserId == DataModel.User.Id)
+                        {
+                            DataModel.VoiceConnection.Speaking = false;
+                        }
+                        break;
+                    case DiscordEvent<SpeakingStartData> speakingStart:
+                        if (speakingStart.Data.UserId == DataModel.User.Id)
+                        {
+                            DataModel.VoiceConnection.Speaking = true;
+                        }
+                        break;
+                    case DiscordEvent<VoiceChannelSelectData> voiceSelect:
+                        if (voiceSelect.Data.ChannelId is not null)//join voice channel
+                        {
+                            DataModel.VoiceConnection.Connected.Trigger();
+                            await UpdateVoiceChannelData();
+                        }
+                        else//leave voice channel
+                        {
+                            DataModel.VoiceConnection.Disconnected.Trigger();
+                        }
+                        break;
+                    default:
+                        _logger.Error("Received unexpected discord event of type {eventType}", discordEvent.Event);
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.Error("Error handling discord event.", e);
             }
         }
 
