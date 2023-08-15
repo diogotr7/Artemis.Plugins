e690004e721bf886f141b82b5d0c3f5a715b3591
Diogo Trindade <diogotr7@gmail.com>
2023-08-15 19:36:28 +0100

    updated chromareader to new library

--- src/Artemis.Plugins.LayerBrushes.Chroma/Services/ChromaService.cs
+++ src/Artemis.Plugins.LayerBrushes.Chroma/Services/ChromaService.cs
@@ -1,23 +1,24 @@
 using Artemis.Core.Services;
-using RazerSdkWrapper;
-using RazerSdkWrapper.Data;
 using Serilog;
 using SkiaSharp;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using RazerSdkReader;
+using RazerSdkReader.Structures;
 
 namespace Artemis.Plugins.LayerBrushes.Chroma.Services;
 
 public class ChromaService : IPluginService, IDisposable
 {
     private readonly ILogger _logger;
-    private readonly RzSdkManager _manager;
+    private readonly ChromaReader _reader;
 
     public event EventHandler<RzDeviceType>? MatrixUpdated;
     public event EventHandler? AppListUpdated;
 
-    public string? CurrentApp { get; private set; } = string.Empty;
+    public int? CurrentAppId { get; private set; } = null;
+    public string? CurrentApp { get; private set; } = null;
     public List<string> Apps { get; } = new();
     public List<int> Pids { get; } = new();
     public ConcurrentDictionary<RzDeviceType, SKColor[,]> Matrices { get; } = new();
@@ -25,105 +26,106 @@ public class ChromaService : IPluginService, IDisposable
     public ChromaService(ILogger logger)
     {
         _logger = logger;
+        _logger.Verbose("Starting RazerSdkReader...");
 
-        _logger.Verbose("Starting RzSDKManager...");
-        _manager = new RzSdkManager()
-        {
-            AppListEnabled = true,
-            MousepadEnabled = true,
-            MouseEnabled = true,
-            KeypadEnabled = true,
-            KeyboardEnabled = true,
-            HeadsetEnabled = true,
-            ChromaLinkEnabled = true
-        };
-        _logger.Verbose("Started RzSdkManager successfully");
-        _manager.DataUpdated += OnDataUpdated;
+        _reader = new();
+        _reader.KeyboardUpdated += RazerEmulatorReaderOnKeyboardUpdated;
+        _reader.MouseUpdated += RazerEmulatorReaderOnMouseUpdated;
+        _reader.MousepadUpdated += RazerEmulatorReaderOnMousepadUpdated;
+        _reader.KeypadUpdated += RazerEmulatorReaderOnKeypadUpdated;
+        _reader.HeadsetUpdated += RazerEmulatorReaderOnHeadsetUpdated;
+        _reader.ChromaLinkUpdated += RazerEmulatorReaderOnChromaLinkUpdated;
+        _reader.AppDataUpdated += RazerEmulatorReaderOnAppDataUpdated;
+        _reader.Start();
+        _logger.Verbose("Started RazerSdkReader successfully");
 
         UpdateAppList(true);
     }
 
+    private void RazerEmulatorReaderOnAppDataUpdated(object? sender, ChromaAppData e)
+    {
+        UpdateAppListData(e);
+    }
+
+    private void RazerEmulatorReaderOnChromaLinkUpdated(object? sender, ChromaLink e)
+    {
+        UpdateMatrix(RzDeviceType.ChromaLink, e);
+    }
+
+    private void RazerEmulatorReaderOnHeadsetUpdated(object? sender, ChromaHeadset e)
+    {
+        UpdateMatrix(RzDeviceType.Headset, e);
+    }
+
+    private void RazerEmulatorReaderOnKeypadUpdated(object? sender, ChromaKeypad e)
+    {
+        UpdateMatrix(RzDeviceType.Keypad, e);
+    }
+
+    private void RazerEmulatorReaderOnMousepadUpdated(object? sender, ChromaMousepad e)
+    {
+        UpdateMatrix(RzDeviceType.Mousepad, e);
+    }
+
+    private void RazerEmulatorReaderOnMouseUpdated(object? sender, ChromaMouse e)
+    {
+        UpdateMatrix(RzDeviceType.Mouse, e);
+    }
+
+    private void RazerEmulatorReaderOnKeyboardUpdated(object? sender, ChromaKeyboard e)
+    {
+        UpdateMatrix(RzDeviceType.Keyboard, e);
+    }
+
     internal void UpdateAppList(bool forced = false)
     {
-        if (CurrentApp == null && !forced)
-            return;
-
-        RzAppListDataProvider applist = _manager.GetDataProvider<RzAppListDataProvider>();
-        applist.Update();
-        UpdateAppListData(applist);
+        //TODO: Is this needed anymore?
+        return;
     }
 
-    private void OnDataUpdated(object? sender, EventArgs e)
+    private void UpdateMatrix<T>(RzDeviceType deviceType, T data) where T : IColorProvider
     {
-        if (sender is not AbstractDataProvider provider)
-            return;
+        var matrix = Matrices.GetOrAdd(deviceType, static (id, g) => new SKColor[g.Height, g.Width], data);
 
-        provider.Update();
-
-        if (provider is RzAppListDataProvider app)
+        for (var i = 0; i < data.Height; i++)
         {
-            UpdateAppListData(app);
-            _logger.Verbose("Updated AppList: CurrentApp: {currentApp} | Apps: {apps} | Pids: {pids}", CurrentApp ?? "None", Apps, Pids);
-        }
-        else if (provider is AbstractColorDataProvider colorProvider)
-        {
-            UpdateMatrix(colorProvider);
-            _logger.Verbose("Updated {provider}. Zone zero: {color}", _deviceTypeDict[colorProvider.GetType()], colorProvider.GetZoneColor(0));
-        }
-    }
-
-    private void UpdateMatrix(AbstractColorDataProvider colorProvider)
-    {
-        RzDeviceType matrixDeviceType = _deviceTypeDict[colorProvider.GetType()];
-        GridSize grid = colorProvider.Grids[0];
-
-        SKColor[,] matrix = Matrices.GetOrAdd(matrixDeviceType, static (id,g) => new SKColor[g.Height, g.Width], grid);
-
-        for (int i = 0; i < grid.Height; i++)
-        {
-            for (int j = 0; j < grid.Width; j++)
+            for (var j = 0; j < data.Width; j++)
             {
-                (byte r, byte g, byte b) = colorProvider.GetZoneColor((int)(i * grid.Width + j));
-                matrix[i, j] = new SKColor(r, g, b);
+                var clr = data.GetColor(i * data.Width + j);
+                matrix[i, j] = new SKColor(clr.R, clr.G, clr.B);
             }
         }
 
-        MatrixUpdated?.Invoke(this, matrixDeviceType);
+        MatrixUpdated?.Invoke(this, deviceType);
     }
 
-    private void UpdateAppListData(RzAppListDataProvider app)
+    private void UpdateAppListData(ChromaAppData app)
     {
         Apps.Clear();
         Pids.Clear();
-        CurrentApp = app.CurrentAppExecutable;
-        for (int i = 0; i < app.AppCount; i++)
+        CurrentAppId = (int?)app.CurrentAppId;
+        CurrentApp = null;
+        for (var i = 0; i < app.AppCount; i++)
         {
-            Apps.Add(app.GetExecutableName(i));
-            Pids.Add(app.GetPid(i));
+            Apps.Add(app.AppInfo[i].AppName);
+            Pids.Add((int)app.AppInfo[i].AppId);
+            if (app.AppInfo[i].AppId == app.CurrentAppId)
+                CurrentApp = app.AppInfo[i].AppName;
         }
 
         AppListUpdated?.Invoke(this, EventArgs.Empty);
+        _logger.Verbose("Updated Chroma app list");
     }
 
-    private readonly Dictionary<Type, RzDeviceType> _deviceTypeDict = new()
-    {
-        [typeof(RzMousepadDataProvider)] = RzDeviceType.Mousepad,
-        [typeof(RzMouseDataProvider)] = RzDeviceType.Mouse,
-        [typeof(RzKeypadDataProvider)] = RzDeviceType.Keypad,
-        [typeof(RzKeyboardDataProvider)] = RzDeviceType.Keyboard,
-        [typeof(RzHeadsetDataProvider)] = RzDeviceType.Headset,
-        [typeof(RzChromaLinkDataProvider)] = RzDeviceType.ChromaLink
-    };
-
-    #region IDisposable
     public void Dispose()
     {
-        _manager.DataUpdated -= OnDataUpdated;
-        //TODO: disposing this throws this:
-        //System.ApplicationException: Object synchronization method was called from an unsynchronized block of code.
-        //idk what to do about it
-        //_manager?.Dispose();
-        GC.SuppressFinalize(this);
+        _reader.KeyboardUpdated -= RazerEmulatorReaderOnKeyboardUpdated;
+        _reader.MouseUpdated -= RazerEmulatorReaderOnMouseUpdated;
+        _reader.MousepadUpdated -= RazerEmulatorReaderOnMousepadUpdated;
+        _reader.KeypadUpdated -= RazerEmulatorReaderOnKeypadUpdated;
+        _reader.HeadsetUpdated -= RazerEmulatorReaderOnHeadsetUpdated;
+        _reader.ChromaLinkUpdated -= RazerEmulatorReaderOnChromaLinkUpdated;
+        _reader.AppDataUpdated -= RazerEmulatorReaderOnAppDataUpdated;
+        _reader.Dispose();
     }
-    #endregion
 }
