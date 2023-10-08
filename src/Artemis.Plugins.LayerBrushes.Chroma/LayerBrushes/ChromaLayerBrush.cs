23368af6193236fa6663a5b7db22ae3b8c6d553b
Diogo Trindade <diogotr7@gmail.com>
2023-10-08 17:08:28 +0100

    Initial experimentation with Chroma link #20

--- src/Artemis.Plugins.LayerBrushes.Chroma/LayerBrushes/ChromaLayerBrush.cs
+++ src/Artemis.Plugins.LayerBrushes.Chroma/LayerBrushes/ChromaLayerBrush.cs
@@ -12,18 +12,24 @@ namespace Artemis.Plugins.LayerBrushes.Chroma.LayerBrushes;
 public class ChromaLayerBrush : PerLedLayerBrush<ChromaPropertyGroup>
 {
     private readonly ChromaService _chroma;
-    private readonly PluginSetting<Dictionary<RzDeviceType, LedId[,]>> _keyMapSetting;
     private readonly Dictionary<LedId, SKColor> _colors;
+    private readonly HashSet<RGBDeviceType> _perLedDeviceTypes;
     private readonly object _lock;
     private bool _shouldRender;
     private bool _playingOverwatch;
 
-    public ChromaLayerBrush(ChromaService chroma, PluginSettings pluginSettings)
+    public ChromaLayerBrush(ChromaService chroma)
     {
         _chroma = chroma;
-        _keyMapSetting = pluginSettings.GetSetting("ChromaKeymap", DefaultChromaLedMap.Clone());
         _colors = new();
         _lock = new();
+        _perLedDeviceTypes = new()
+        {
+            RGBDeviceType.Keyboard,
+            RGBDeviceType.Mouse,
+            RGBDeviceType.Mousepad,
+            RGBDeviceType.Headset
+        };
     }
 
     public override void EnableLayerBrush()
@@ -47,8 +53,21 @@ public class ChromaLayerBrush : PerLedLayerBrush<ChromaPropertyGroup>
 
     private void OnMatrixUpdated(object? sender, MatrixUpdatedEventArgs args)
     {
+        if (args.DeviceType == RzDeviceType.ChromaLink)
+        {
+            for (var i = 0; i < args.Matrix.GetLength(0); i++)
+            {
+                for (var j = 0; j < args.Matrix.GetLength(1); j++)
+                {
+                    _chromaLink[i * 5 + j] = args.Matrix[i, j];
+                }
+            }
+            
+            return;
+        }
+        
         var matrix = args.Matrix;
-        var dict = _keyMapSetting.Value![args.DeviceType];
+        var dict = DefaultChromaLedMap.DeviceTypes[args.DeviceType];
 
         lock (_lock)
         {
@@ -71,8 +90,14 @@ public class ChromaLayerBrush : PerLedLayerBrush<ChromaPropertyGroup>
 
         lock (_lock)
         {
+            if (!_perLedDeviceTypes.Contains(led.Device.DeviceType))
+                return GetLedForDevice(led);
+            
             if (_colors.TryGetValue(led.RgbLed.Id, out var color))
                 return ProcessColor(color);
+            
+            if (Properties.UseDefaultLed && TryGetDefaultLed(led, out var defaultLed))
+                return ProcessColor(defaultLed);
             
             //According to razer docs, chromaLink1 is the "catchall" ledId. If an LED doesn't have a mapping, use this color.
             if (Properties.UseDefaultLed && _colors.TryGetValue(LedId.LedStripe1, out var chromaLink1))
@@ -81,6 +106,19 @@ public class ChromaLayerBrush : PerLedLayerBrush<ChromaPropertyGroup>
 
         return SKColor.Empty;
     }
+
+    /// <summary>
+    ///     Gets the default color for the provided LED based on the device type
+    /// </summary>
+    private bool TryGetDefaultLed(ArtemisLed led, out SKColor color) => led.Device.DeviceType switch
+    {
+        RGBDeviceType.Keyboard => _colors.TryGetValue(LedId.Keyboard_Space, out color),
+        RGBDeviceType.Mouse => _colors.TryGetValue(LedId.Mouse1, out color),
+        RGBDeviceType.Mousepad => _colors.TryGetValue(LedId.Mousepad1, out color),
+        RGBDeviceType.Headset => _colors.TryGetValue(LedId.Headset1, out color),
+        RGBDeviceType.Keypad => _colors.TryGetValue(LedId.Keypad1, out color),
+        _ => _colors.TryGetValue(LedId.LedStripe1, out color)
+    };
 
     private SKColor ProcessColor(SKColor color)
     {
@@ -91,5 +129,17 @@ public class ChromaLayerBrush : PerLedLayerBrush<ChromaPropertyGroup>
             return correctedColor;
 
         return color;
+    }
+
+    private readonly SKColor[] _chromaLink = new SKColor[5];
+    private SKColor GetLedForDevice(ArtemisLed led)
+    {
+        //if we get here, the device should be colored with animated chroma link
+        var ledIndex = led.Device.Leds.IndexOf(led);
+        
+        //chroma link has 4 LEDs
+        var chromaLinkIndex = ledIndex % 5;
+        
+        return _chromaLink[chromaLinkIndex];
     }
 }
